cb6ff2044ec03dafb825df2090075c9b1e453eb0
KamilKaczmarkiewicz <kaczmarkiewicz98@gmail.com>
2025-04-08 22:37:51 +0200

    added / in the beginning RequestMapping for PersonController

--- src/main/java/com/project/skill/person/PersonController.java
+++ src/main/java/com/project/skill/person/PersonController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/v1/persons")
+@RequestMapping("/api/v1/persons")
 @RequiredArgsConstructor
 class PersonController {
 
